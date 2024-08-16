e5f1d0bfd732b383ab9b38a6f1f7ba0b58534df3
LITinx <kuruchbekov2006dan@gmail.com>
2024-08-16 23:50:17 +0600

    refactor: add user component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,33 +3,41 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AdminComponent } from './admin/pages/admin.component';
 import { NotFoundComponent } from './core/pages/not-found/not-found.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () =>
-      import('./search/search.module').then(m => m.SearchModule),
-  },
-  {
-    path: 'signin',
-    loadChildren: () => import('./login/login.module').then(m => m.LoginModule),
-  },
-  {
-    path: 'signup',
-    loadChildren: () =>
-      import('./registration/registration.module').then(
-        m => m.RegistrationModule
-      ),
-  },
-  {
-    path: 'profile',
-    loadChildren: () =>
-      import('./profile/profile.module').then(m => m.ProfileModule),
-  },
-  {
-    path: 'orders',
-    loadChildren: () =>
-      import('./orders/orders.module').then(m => m.OrdersModule),
+    component: UserComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./search/search.module').then(m => m.SearchModule),
+      },
+      {
+        path: 'signin',
+        loadChildren: () =>
+          import('./login/login.module').then(m => m.LoginModule),
+      },
+      {
+        path: 'signup',
+        loadChildren: () =>
+          import('./registration/registration.module').then(
+            m => m.RegistrationModule
+          ),
+      },
+      {
+        path: 'profile',
+        loadChildren: () =>
+          import('./profile/profile.module').then(m => m.ProfileModule),
+      },
+      {
+        path: 'orders',
+        loadChildren: () =>
+          import('./orders/orders.module').then(m => m.OrdersModule),
+      },
+    ],
   },
   {
     path: 'admin',
