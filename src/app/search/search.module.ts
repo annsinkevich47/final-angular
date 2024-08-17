a8809d099465ad2871ba8e49d8f3876f5f58b061
unknown <kirill.zayats.99@mail.ru>
2024-08-17 11:32:46 +0300

    feat: add main components on the search page

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -5,9 +5,12 @@ import { SharedModule } from '../shared/shared.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { TripPageComponent } from './pages/trip-page/trip-page.component';
 import { SearchRoutingModule } from './search-routing.module';
+import { SearchComponent } from './components/search/search.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { ResultComponent } from './components/result/result.component';
 
 @NgModule({
-  declarations: [HomePageComponent, TripPageComponent],
+  declarations: [HomePageComponent, TripPageComponent, SearchComponent, FilterComponent, ResultComponent],
   imports: [CommonModule, SharedModule, SearchRoutingModule],
 })
 export class SearchModule {}
