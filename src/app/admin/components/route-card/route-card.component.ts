91d21214b956eb359ac1e7f1e77db8d54945dcca
LITinx <kuruchbekov2006dan@gmail.com>
2024-08-22 00:31:41 +0600

    feat: show city names not ids

--- src/app/admin/components/route-card/route-card.component.ts
+++ src/app/admin/components/route-card/route-card.component.ts
@@ -1,16 +1,37 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteRouteModalComponent } from '../../../shared/components/delete-route-modal/delete-route-modal.component';
 import { RouteType } from '../../../shared/models/routes-response.model';
+import { Station } from '../../../shared/models/stations-response.model';
+import { StationService } from '../../services/station.service';
 
 @Component({
   selector: 'app-route-card',
   templateUrl: './route-card.component.html',
   styleUrl: './route-card.component.scss',
 })
-export class RouteCardComponent {
+export class RouteCardComponent implements OnInit {
   @Input() route: RouteType;
+  stations: Station[];
   readonly dialog = inject(MatDialog);
+
+  constructor(private stationService: StationService) {}
+
+  ngOnInit(): void {
+    this.stationService.getStations().subscribe(data => (this.stations = data));
+  }
+
+  getPathNames() {
+    return this.route.path
+      .map(id => {
+        const station = this.stations.find(station => station.id === id);
+        if (station) {
+          return station.city;
+        }
+        return null;
+      })
+      .filter(station => station !== null) as string[];
+  }
 
   openDialog(): void {
     this.dialog.open(DeleteRouteModalComponent, {
