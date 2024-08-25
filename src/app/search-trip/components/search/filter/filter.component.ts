393ffcb0c9e082a141c7de80802e161d8631af5b
unknown <kirill.zayats.99@mail.ru>
2024-08-25 18:23:54 +0300

    refactor: bug about search and fix: naming

--- src/app/search-trip/components/search/filter/filter.component.ts
+++ src/app/search-trip/components/search/filter/filter.component.ts
@@ -33,6 +33,7 @@ export class FilterComponent implements OnInit, OnDestroy {
   }
 
   private generateCards(arrayDate: string[]): void {
+    this.idActiveCard = 0;
     this.cards.length = 0;
     for (let index = 0; index < arrayDate.length; index += 1) {
       const { date, dayName } = getDaydate(arrayDate[index]);
--- src/app/search-trip/services/search.service.ts
+++ src/app/search-trip/services/search.service.ts
@@ -155,8 +155,6 @@ export class SearchService {
     copyIndexFrom: number,
     copyIndexTo: number,
   ): { arrayTime: string[]; arrayDuration: string[] } {
-    console.log(schedule);
-
     const arrayTime: string[] = [];
     const arrayDuration: string[] = [];
     for (
@@ -314,17 +312,17 @@ export class SearchService {
     indexFrom: number,
     indexTo: number,
   ): number[] {
-    const arrauPrices: number[] = [0, 0, 0, 0, 0, 0];
+    const arrayPrices: number[] = [0, 0, 0, 0, 0, 0];
 
     for (let index = indexFrom; index < indexTo; index += 1) {
       ArrayTypePrices.forEach((type, indexArrayPrices) => {
-        arrauPrices[indexArrayPrices] += schedule.segments[index].price[type]
+        arrayPrices[indexArrayPrices] += schedule.segments[index].price[type]
           ? schedule.segments[index].price[type]
           : 0;
       });
     }
 
-    return arrauPrices;
+    return arrayPrices;
   }
 
   private createCardStation(
