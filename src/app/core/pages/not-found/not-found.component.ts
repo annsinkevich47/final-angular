6a5e676074eef7f27995d3f8ffd977d1071a199f
adAstra97 <131522998+adAstra97@users.noreply.github.com>
2024-08-25 12:45:25 +0300

    feat: add not found page

--- src/app/core/pages/not-found/not-found.component.ts
+++ src/app/core/pages/not-found/not-found.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
 })
-export class NotFoundComponent {}
+export class NotFoundComponent {
+  constructor(private router: Router) {}
+
+  public navigateToHome(): void {
+    this.router.navigateByUrl('/');
+  }
+}
